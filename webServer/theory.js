4c1ee2b75dc5b0030c20f1a5079c6a7c8f3ab590
CoolShivas <shivas2710cool00@gmail.com>
2025-03-12 16:17:55 +0530

    Theory and Introduction to package.json.

---
+++ webServer/theory.js
@@ -0,0 +1,37 @@
+/**
+ * 
+ * * * * * Introduction to package.json :-
+ * 
+ * 
+    1.) package.json is a configuration file used in Node.js projects.
+
+    2.) It contains metadata about the project and information on project dependencies.
+
+    3.) Go to your project folder and use npm init to initialize the project or 
+        to create package.json.
+
+    4.) Name, Version, Description (”name”, “version”, “description”):
+    
+        • Specifies the name and version of the project.
+    
+        • Helps uniquely identify and version the project.
+    
+    5.) Entry Point (”main”):
+ 
+        • Specifies the main entry point file for the application.
+ 
+        • The file executed when the application starts 
+
+    6.) Scripts:
+        • Contains custom scripts for various tasks (e.g., running tests, starting the server).
+        • Provides shortcuts for common development tasks
+
+    7.) Author, License, and More:
+        • Information about the project's author, license, repository, and other metadata.
+        • Useful for documentation and collaboration.
+ 
+    8.) You can also use npm init -y to create package.json quickly
+    
+ * 
+ * 
+ */ 
