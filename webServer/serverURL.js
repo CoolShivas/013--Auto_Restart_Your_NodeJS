fdb040fa5ed28603177d79e923152c707240fa15
CoolShivas <shivas2710cool00@gmail.com>
2025-03-13 12:17:31 +0530

    Finally, Auto Restart of Server.

--- webServer/serverURL.js
+++ webServer/serverURL.js
@@ -13,11 +13,17 @@ const urlServer = http.createServer((request, response) => {
     if(request.url === "/contact")
     {
         response.setHeader("Content-Type", "text/html");
-        response.write("<center> Feel free to contact us. <center/>");
+        response.write("<center> Feel free to contact us 24*7. <center/>");
         response.end();
     }
     // // // Here, the new page added i.e., "/contact" and saved but on localhost:5000/contact is not showing and buffering is going on.
     // // // It is happening because of PORT is not refresh and not showing the updated page i.e., "/contact". So, everytime save the code first and then open the Terminal then press ctrl C to stop the previous code/program. And, again restart it. You will get the result.
+    if(request.url === "/about")
+        {
+            response.setHeader("Content-Type", "text/html");
+            response.write("<textarea> Welcome to about page. Do you want to know more about us. Then, we will not discuss. <textarea/>");
+            response.end();
+        }
 });
 
 
@@ -36,3 +42,19 @@ urlServer.listen(PORT, () => {
 // // // // Now, enter the url localhost:5000/contact 
 // // // // Therefore, on Desktop we are able to see the Output as :- 
 // // // // Feel free to contact us.
+
+
+// // // // Finally, after the installation of Nodemon and also globally install. So, we are able to update data on real time without running again and again node serverURL.js to ckeck the reflected updates on the Browser. 
+
+// // // // After installation then open the terminal and write the command ctrl + C to stop the previous server (i.e., node serverURL.js) and now use nodemon serverURL.js it will show the Output as :-
+/*
+// [nodemon] 3.1.9
+// [nodemon] to restart at any time, enter `rs`
+// [nodemon] watching path(s): *.*
+// [nodemon] watching extensions: js,mjs,cjs,json
+// [nodemon] starting `node serverURL.js`
+// Listening to Port, 5000
+*/
+
+// // // // Now, on every update there in no need of this command node serverURL.js again and again. 
+// // // // Therefore, Nodemon automatically update the server or PORT to restart after any update is save.
